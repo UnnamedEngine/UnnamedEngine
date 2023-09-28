d5979d7c889b23fd9d68c613c81cc85c16003d2b
ZotyDev <leonardokronbauer2002@gmail.com>
2023-09-28 15:31:10 -0300

    Added barebones winit|wgpu window

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,8 @@
 - Start of the project
 - Added github actions to build and run tests on Windows, Ubuntu and MacOS
 - Barebones entry point
+- Server project
+- Client project
 - Added the branding
 - Added `README.md`
 - Added `LICENSE.md` (MIT)
--- unnamed_engine/src/core/engine.rs
+++ unnamed_engine/src/core/engine.rs
@@ -1,3 +1,13 @@
+use super::state::State;
+
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+use tokio::task;
+
 pub struct Engine {
     running: bool,
     title: String,
@@ -7,17 +17,19 @@ impl Engine {
     pub fn new(title: String) -> Self {
         Engine {
             running: false,
-            title: title
+            title: title,
         }
     }
 
     // Starts the engine
     // This method is the only that should be called from the application
     pub fn start(&mut self, start_f: impl FnOnce(), update_f: impl FnMut(), render_f: impl FnMut()) {
+        env_logger::init();
+
         start_f();
 
         self.running = true;
-        self.run(update_f, render_f);
+        let result = tokio::runtime::Runtime::new().unwrap().block_on(self.run(update_f, render_f));
     }
 
     // // Stops the engine
@@ -28,11 +40,57 @@ impl Engine {
     // }
 
     // Starts running the engine
-    fn run(&self, mut update_f: impl FnMut(), mut render_f: impl FnMut()) {
-        while self.running {
-            self.update(&mut update_f);
-            self.render(&mut render_f);
-        }
+    async fn run(&self, mut update_f: impl FnMut(), mut render_f: impl FnMut()) {
+        let event_loop = EventLoop::new();
+        let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+        let mut state = State::new(window).await;
+
+        let my_window_id = state.window().id();
+
+        event_loop.run(move |event, _, control_flow| {
+            match event {
+                Event::WindowEvent {
+                    ref event,
+                    window_id,
+                } if window_id == my_window_id => if !state.input(event) {
+                    match event {
+                        WindowEvent::CloseRequested | WindowEvent::KeyboardInput {
+                            input: KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
+                            ..
+                        } => *control_flow = ControlFlow::Exit,
+                        WindowEvent::Resized(physical_size) => {
+                            state.resize(*physical_size);
+                        },
+                        WindowEvent::ScaleFactorChanged { new_inner_size, ..} => {
+                            state.resize(**new_inner_size);
+                        }
+                        _ => {}
+                    }
+                },
+                Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+                    state.update();
+                    match state.render() {
+                        Ok(_) => {}
+                        // Reconfigure the surface if lost
+                        Err(wgpu::SurfaceError::Lost) => state.resize(*state.size()),
+                        // The system is out of memory, we should probably quit
+                        Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                        // All other errors (Outdated, Timeout) should be resolved by the next frame
+                        Err(e) => eprintln!("{:?}", e),
+                    }
+                },
+                Event::MainEventsCleared => {
+                    // RedrawRequested will only trigger once, unless we manually request it
+                    state.window().request_redraw();
+                }
+                _ => {}
+            }
+        });
     }
 
     // Logical update that runs at each iteration of the engine
