9d0e3fc9883daf0a7b45926889b1f579988ec8b7
ZotyDev <leonardokronbauer2002@gmail.com>
2023-10-06 17:54:23 -0300

    Added index buffers

--- unnamed_engine/src/core/state.rs
+++ unnamed_engine/src/core/state.rs
@@ -30,9 +30,15 @@ impl Vertex {
 }
 
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
-    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
-    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0]},
+    Vertex { position: [-0.5, -0.5, 0.0], color: [1.0, 0.0, 0.5] },
+    Vertex { position: [0.5, -0.5, 0.0],  color: [0.0, 1.0, 0.5] },
+    Vertex { position: [-0.5, 0.5, 0.0],  color: [0.0, 1.0, 0.5] },
+    Vertex { position: [0.5, 0.5, 0.0], color: [1.0, 0.0, 0.5]   },
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 2,
+    2, 1, 3,
 ];
 
 pub struct State {
@@ -44,7 +50,8 @@ pub struct State {
     window: Window,
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
-    num_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
 }
 
 impl State {
@@ -153,7 +160,15 @@ impl State {
             }
         );
 
-        let num_vertices = VERTICES.len() as u32;
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            }
+        );
+
+        let num_indices = INDICES.len() as u32;
 
         Self {
             window,
@@ -164,7 +179,8 @@ impl State {
             size,
             render_pipeline,
             vertex_buffer,
-            num_vertices,
+            index_buffer,
+            num_indices,
         }
     }
 
@@ -225,7 +241,8 @@ impl State {
 
             render_pass.set_pipeline(&self.render_pipeline);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.num_vertices, 0..1);
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
